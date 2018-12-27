3cca37d0bb570922dafddb49b9a32fa3a2edd42e
Francisco <franciscobritomail@gmail.com>
2018-12-27 12:43:27 +0000

    corrected directory structure

--- copy-temp-data.sh
+++ copy-temp-data.sh
@@ -15,74 +15,106 @@ ny=$(awk 'BEGIN{FS=","; RS=""} {print $24}' temp-data/simulationParameters.csv)
 if [ $geom -eq 1 ]
 then
   mkdir examples/data/1d-chain-pbc/
-  mkdir examples/data/1d-chain-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
-  cp -r temp-data/* examples/data/1d-chain-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
+  mkdir examples/data/1d-chain-pbc/N$nsites-BETA$beta-U$U-MU$mu
+  mkdir examples/plots/1d-chain-pbc/
+  mkdir examples/plots/1d-chain-pbc/N$nsites-BETA$beta-U$U-MU$mu
+  cp -r temp-data/* examples/data/1d-chain-pbc/N$nsites-BETA$beta-U$U-MU$mu
 fi
 
 if [ $geom -eq 2 ]
 then
-  mkdir examples/data/1d-chain-obc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
-  cp -r temp-data/* examples/data/1d-chain-obc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
+  mkdir examples/data/1d-chain-obc/
+  mkdir examples/data/1d-chain-obc/N$nsites-BETA$beta-U$U-MU$mu
+  mkdir examples/plots/1d-chain-obc/
+  mkdir examples/plots/1d-chain-obc/N$nsites-BETA$beta-U$U-MU$mu
+  cp -r temp-data/* examples/data/1d-chain-obc/N$nsites-BETA$beta-U$U-MU$mu
 fi
 
 if [ $geom -eq 3 ]
 then
-  mkdir examples/data/2d-sq-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
-  cp -r temp-data/* examples/data/2d-sq-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
+  mkdir examples/data/2d-sq-pbc/
+  mkdir examples/data/2d-sq-pbc/N$nsites-BETA$beta-U$U-MU$mu
+  mkdir examples/plots/2d-sq-pbc/
+  mkdir examples/plots/2d-sq-pbc/N$nsites-BETA$beta-U$U-MU$mu
+  cp -r temp-data/* examples/data/2d-sq-pbc/N$nsites-BETA$beta-U$U-MU$mu
 fi
 
 if [ $geom -eq 4 ]
 then
-  mkdir examples/data/2d-sq-obc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
-  cp -r temp-data/* examples/data/2d-sq-obc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
+  mkdir examples/data/2d-sq-obc/
+  mkdir examples/data/2d-sq-obc/N$nsites-BETA$beta-U$U-MU$mu
+  mkdir examples/plots/2d-sq-obc/
+  mkdir examples/plots/2d-sq-obc/N$nsites-BETA$beta-U$U-MU$mu
+  cp -r temp-data/* examples/data/2d-sq-obc/N$nsites-BETA$beta-U$U-MU$mu
 fi
 
 if [ $geom -eq 5 ]
 then
+  mkdir examples/data/2d-rec-pbc/
   mkdir examples/data/2d-rec-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
+  mkdir examples/plots/2d-rec-pbc/
+  mkdir examples/plots/2d-rec-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
   cp -r temp-data/* examples/data/2d-rec-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
 fi
 
 if [ $geom -eq 6 ]
 then
+  mkdir examples/data/2d-rec-obc/
   mkdir examples/data/2d-rec-obc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
+  mkdir examples/plots/2d-rec-obc/
+  mkdir examples/plots/2d-rec-obc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
   cp -r temp-data/* examples/data/2d-rec-obc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
 fi
 
 if [ $geom -eq 7 ]
 then
+  mkdir examples/data/2d-triang-pbc/
   mkdir examples/data/2d-triang-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
+  mkdir examples/plots/2d-triang-pbc/
+  mkdir examples/plots/2d-triang-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
   cp -r temp-data/* examples/data/2d-triang-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
 fi
 
 if [ $geom -eq 8 ]
 then
+  mkdir examples/data/2d-triang-nanoribbon/
   mkdir examples/data/2d-triang-nanoribbon/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
+  mkdir examples/plots/2d-triang-nanoribbon/
+  mkdir examples/plots/2d-triang-nanoribbon/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
   cp -r temp-data/* examples/data/2d-triang-nanoribbon/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
 fi
 
 if [ $geom -eq 9 ]
 then
-  mkdir examples/data/2d-hc-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
-  cp -r temp-data/* examples/data/2d-hc-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
+  mkdir examples/data/2d-hc-pbc/
+  mkdir examples/data/2d-hc-pbc/N$nsites-BETA$beta-U$U-MU$mu
+  mkdir examples/plots/2d-hc-pbc/
+  mkdir examples/plots/2d-hc-pbc/N$nsites-BETA$beta-U$U-MU$mu
+  cp -r temp-data/* examples/data/2d-hc-pbc/N$nsites-BETA$beta-U$U-MU$mu
 fi
 
 if [ $geom -eq 10 ]
 then
+  mkdir examples/data/2d-hc-nanoribbon/
   mkdir examples/data/2d-hc-nanoribbon/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
+  mkdir examples/plots/2d-hc-nanoribbon/
+  mkdir examples/plots/2d-hc-nanoribbon/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
   cp -r temp-data/* examples/data/2d-hc-nanoribbon/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
 fi
 
 if [ $geom -eq 11 ]
 then
+  mkdir examples/data/2d-hc-nanoribbon-strain/
   mkdir examples/data/2d-hc-nanoribbon-strain/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
+  mkdir examples/plots/2d-hc-nanoribbon-strain/
+  mkdir examples/plots/2d-hc-nanoribbon-strain/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
   cp -r temp-data/* examples/data/2d-hc-nanoribbon-strain/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
 fi
 
 if [ $geom -eq 12 ]
 then
-  mkdir examples/data/2d-tmd-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
-  cp -r temp-data/* examples/data/2d-tmd-pbc/N$nsites-BETA$beta-U$U-MU$mu-NY$ny
+  mkdir examples/data/2d-tmd-pbc/N$nsites-BETA$beta-U$U-MU$mu
+  cp -r temp-data/* examples/data/2d-tmd-pbc/N$nsites-BETA$beta-U$U-MU$mu
 fi
 
 if [ $geom -eq 13 ]
